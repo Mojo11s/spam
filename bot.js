4e304f3ab556a60cb75cd30cbec28d01f1f7117d
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-10-31 14:01:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("639164897166622730")
+let channel =     client.channels.get("639428699405680651")
 setInterval(function() {
 channel.send(`كسم اي حد يعترض طريقي``);
 }, 30)
