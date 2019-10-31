284c91c04bd1d5d9b671b926b21a57797c3f9c95
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-10-31 13:28:34 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("562584649163997190")
+setInterval(function() {
+channel.send(`كسم اي حد يعترض طريقي``);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
