3538db2dc05b66a4ee6b6f4a8e55e34dcce2a92f
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-10-31 14:13:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("639428699405680651")
 setInterval(function() {
-channel.send(`كسم اي حد يعترض طريقي`);
+channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
 }, 30)
 })
 
