4d60db3172284f4b15292f4848fc59e366acb5e9
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-11-26 22:37:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const client2 = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
@@ -10,4 +11,13 @@ channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+client2.on("ready", () => {
+let channel =     client2.channels.get("639428699405680651")
+setInterval(function() {
+channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
+}, 30)
+})
+
+
+client.login(process.env.BOT_TOKEN1);
+client2.login(process.env.BOT_TOKEN2);
