989c186e6f7a70658b8257c164f005d59875ef69
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-11-26 22:48:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
@@ -18,6 +19,21 @@ channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح
 }, 30)
 })
 
+client3.on("ready", () => {
+let channel =     client3.channels.get("639428699405680651")
+setInterval(function() {
+channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
+}, 30)
+})
+
+client4.on("ready", () => {
+let channel =     client4.channels.get("639428699405680651")
+setInterval(function() {
+channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
+}, 30)
+})
 
 client.login(process.env.BOT_TOKEN1);
 client2.login(process.env.BOT_TOKEN2);
+client3.login(process.env.BOT_TOKEN3);
+client4.login(process.env.BOT_TOKEN4);
