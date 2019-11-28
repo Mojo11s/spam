7868566c9422d7169ec9971f5dab06f651ca1ae2
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-11-28 21:23:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ if ( x < 90000 ){
 client.on("ready", () => {
 let channel =     client.channels.get("639428699405680651")
 setInterval(function() {
-channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
+channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي [ "${x}" ] `);
 }, 30)
 })  
 
