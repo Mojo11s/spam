3ffa9d0a1d4476a4284c6e2aa2c0da0d65e3bb01
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-11-28 16:50:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,9 +20,9 @@ client.on("message", function(message) {
     .split(" ")
     .slice(1)
     .join(" ");
-  if (message.content.startsWith(prefix + "say")) {
+  if (message.content.startsWith(prefix + "say1")) {
     if (!args) return;
-    message.channel.send(`**# ${args}**`);
+    message.channel.send(`${args}`);
   }
 });
 
@@ -33,6 +33,19 @@ channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح
 }, 30)
 })
 
+client2.on("message", function(message) {
+  let prefix = "-";
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "say2")) {
+    if (!args) return;
+    message.channel.send(`${args}`);
+  }
+});
+
+
 client3.on("ready", () => {
 let channel =     client3.channels.get("639428699405680651")
 setInterval(function() {
@@ -40,12 +53,38 @@ channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح
 }, 30)
 })
 
+client3.on("message", function(message) {
+  let prefix = "-";
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "say3")) {
+    if (!args) return;
+    message.channel.send(`${args}`);
+  }
+});
+
+
 client4.on("ready", () => {
 let channel =     client4.channels.get("639428699405680651")
 setInterval(function() {
 channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
 }, 30)
 })
+
+client4.on("message", function(message) {
+  let prefix = "-";
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (message.content.startsWith(prefix + "say4")) {
+    if (!args) return;
+    message.channel.send(`${args}`);
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN1);
 client2.login(process.env.BOT_TOKEN2);
