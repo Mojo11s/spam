15d4c2605bdc3371294395e1032ed34f6131f814
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-11-28 17:23:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,27 @@ const prefix = ['-'];
 console.log("Scrpit By Dream");
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("639428699405680651")
+client1.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+client.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
 setInterval(function() {
 channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
 }, 30)
